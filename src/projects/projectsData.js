1aa3a369f435715fd6df7fb692850b9db6883340
ananyagupta44 <ananyag.0801@gmail.com>
2026-05-13 12:56:07 +0530

    Fix unused imports

--- src/projects/projectsData.js
+++ src/projects/projectsData.js
@@ -1,6 +1,6 @@
 import projectOne from "../assets/project-1.png";
 import projectTwo from "../assets/project-2.png";
-import projectThree from "../assets/project-3.png";
+import projectThree from "../assets/project-3.jpeg";
 
 const projects = {
   1: {
@@ -9,7 +9,11 @@ const projects = {
     description: (
       <>
         <p>
-          A responsive and interactive personal portfolio built using React. It showcases my projects, skills, and experience with smooth navigation, modern UI components, and optimized performance. The site highlights my work in an organized and visually appealing manner, reflecting both my technical skills and design sense.
+          A responsive and interactive personal portfolio built using React. It
+          showcases my projects, skills, and experience with smooth navigation,
+          modern UI components, and optimized performance. The site highlights
+          my work in an organized and visually appealing manner, reflecting both
+          my technical skills and design sense.
         </p>
       </>
     ),
@@ -22,8 +26,10 @@ const projects = {
     description: (
       <>
         <p>
-          A dynamic web application made using React.js and Tailwind CSS that lets users search, browse, and save GIFs using the Giphy API. It features category-based filtering, a favorites section, and a responsive, modern UI inspired by the official Giphy platform.
-
+          A dynamic web application made using React.js and Tailwind CSS that
+          lets users search, browse, and save GIFs using the Giphy API. It
+          features category-based filtering, a favorites section, and a
+          responsive, modern UI inspired by the official Giphy platform.
         </p>
       </>
     ),
@@ -31,17 +37,21 @@ const projects = {
     demo: "https://netlify.com",
   },
   3: {
-    title: "Food Delivery App",
+    title: "SENSAI App",
     image: projectThree,
     description: (
       <>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          SENSAI is an AI-powered career development platform designed to help
+          users make smarter career decisions through personalized guidance and
+          data-driven insights. The platform uses the Gemini API to analyze user
+          profiles, identify skill gaps, and recommend suitable career paths,
+          learning resources, and improvement strategies tailored to individual
+          goals.
         </p>
       </>
     ),
-    github: "https://github.com",
+    github: "https://github.com/ananyagupta44/SENSAI-app",
     demo: "https://netlify.com",
   },
 };
